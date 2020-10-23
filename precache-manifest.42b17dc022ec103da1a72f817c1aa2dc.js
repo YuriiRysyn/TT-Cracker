8be3abd13edbe00c8c37af5cf9f1a2983263aafd
Yurii Rysyn <zerocool_cs@ukr.net>
2020-10-23 22:13:43 +0300

    Updates

---
+++ precache-manifest.42b17dc022ec103da1a72f817c1aa2dc.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f6b8d92ae311c75b2204466e263840",
+    "url": "/TT-Cracker/index.html"
+  },
+  {
+    "revision": "026e0355dbdbf6c75382",
+    "url": "/TT-Cracker/static/css/main.a2d374ba.chunk.css"
+  },
+  {
+    "revision": "ea9ce9b261c0c4e26f6b",
+    "url": "/TT-Cracker/static/js/2.f149fa07.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/TT-Cracker/static/js/2.f149fa07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026e0355dbdbf6c75382",
+    "url": "/TT-Cracker/static/js/main.b28bd835.chunk.js"
+  },
+  {
+    "revision": "eee0e92faecca0b7bf68",
+    "url": "/TT-Cracker/static/js/runtime-main.d78db8a4.js"
+  },
+  {
+    "revision": "e9779437dbbe4054969df17e0b55e34f",
+    "url": "/TT-Cracker/static/media/2nd-screen-bg.e9779437.jpg"
+  },
+  {
+    "revision": "899d037e60e652296b4126b7014b7d47",
+    "url": "/TT-Cracker/static/media/About-cracker.899d037e.svg"
+  },
+  {
+    "revision": "4a2f2ea45a0bb1abe81b47d0afde4aae",
+    "url": "/TT-Cracker/static/media/FredokaOne-Regular.4a2f2ea4.ttf"
+  },
+  {
+    "revision": "6929f4d218426ce45c7d2165b9c73ae5",
+    "url": "/TT-Cracker/static/media/Kristi-Regular.6929f4d2.ttf"
+  },
+  {
+    "revision": "db55432096f2b5f2417b578693ebef45",
+    "url": "/TT-Cracker/static/media/LatoBold.db554320.woff2"
+  },
+  {
+    "revision": "632b2f7701c5e447a5c9d44d9471bef5",
+    "url": "/TT-Cracker/static/media/LatoRegular.632b2f77.woff2"
+  },
+  {
+    "revision": "b3be96cbbfd0ff1f3ba3c7c2f58c1441",
+    "url": "/TT-Cracker/static/media/Roboto-Regular-webfont.b3be96cb.woff"
+  },
+  {
+    "revision": "1d5db60a9e91f5179da20872612ef6f9",
+    "url": "/TT-Cracker/static/media/basket.1d5db60a.svg"
+  },
+  {
+    "revision": "5a485ed77b1cc3c792792c7d0c63c881",
+    "url": "/TT-Cracker/static/media/corn.5a485ed7.svg"
+  },
+  {
+    "revision": "af3b54f85c838d140f597c607921bb81",
+    "url": "/TT-Cracker/static/media/details.af3b54f8.svg"
+  },
+  {
+    "revision": "fff8339048fc3f7932f4e3a65f3b1f16",
+    "url": "/TT-Cracker/static/media/fa-share-alt.fff83390.svg"
+  },
+  {
+    "revision": "2e2a231e8a71ad56f1442260e19375cd",
+    "url": "/TT-Cracker/static/media/first-screen-bg.2e2a231e.png"
+  },
+  {
+    "revision": "17777992d208bd14e16c9d9f9edfe10f",
+    "url": "/TT-Cracker/static/media/footer.17777992.svg"
+  },
+  {
+    "revision": "6158e9de069c16a3f85564c51672fdb5",
+    "url": "/TT-Cracker/static/media/map-marker.6158e9de.svg"
+  },
+  {
+    "revision": "3caf03af3dc2ad190b6d1fb97e3dc890",
+    "url": "/TT-Cracker/static/media/pacage.3caf03af.svg"
+  },
+  {
+    "revision": "80c1d5347294cc1c9d4017aeda0e4e71",
+    "url": "/TT-Cracker/static/media/phone.80c1d534.svg"
+  },
+  {
+    "revision": "92e0dadf1be34049c3bc83d92d6eac85",
+    "url": "/TT-Cracker/static/media/sesame.92e0dadf.svg"
+  },
+  {
+    "revision": "8059629758e536f31b3bf9d5fca5ef4c",
+    "url": "/TT-Cracker/static/media/soybean.80596297.svg"
+  },
+  {
+    "revision": "e2d1f30ca6136a856dbfb307aedf78d8",
+    "url": "/TT-Cracker/static/media/wheat.e2d1f30c.svg"
+  }
+]);
